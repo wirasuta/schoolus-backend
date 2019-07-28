18d8f509c634b9f7cc2a18d8af58598df6cf05c2
I Putu Gede Wirasuta <wirasutat@gmail.com>
2019-07-28 23:30:36 +0700

    removed unique

--- models/User.js
+++ models/User.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
   user_name: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   full_name: {
     type: String,
